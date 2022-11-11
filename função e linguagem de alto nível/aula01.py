5b19bbcd9318270bb79148bf72e09a94eb73c78c
Marcel Moura da Luz Melo <marceldaluzmelo@hotmail.com>
2022-11-11 10:16:05 -0300

    atualizando

---
+++ função e linguagem de alto nível/aula01.py
@@ -0,0 +1,18 @@
+def saudacao():
+    primeiro_nome = input('Qual o seu nome? ')
+    sobrenome = input(f'Qual seu sobrenome? ')
+    nome_inteiro = primeiro_nome + sobrenome
+    print(f'Olá {primeiro_nome} {sobrenome}')
+    idade = input('Qual a sua idade? ')
+    print(f'Você tem {idade} anos')
+
+saudacao()
+
+#Linguagens de alto nível são parecidas ou semelhantes com as linguagens
+#humanas.
+
+#Vantagens => mais fácil de aprender
+
+#Linguagens de baixo nível está mais próximo com a linguagem de máquina.
+
+#Vantagens => melhor aproveitamento e desempenho da máquina.
